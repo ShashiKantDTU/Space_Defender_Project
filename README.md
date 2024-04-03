1f8a3ddfdcd750b0398a3d43aacb89d6b83c5267
Shashi Kant <134543343+ShashiKantDTU@users.noreply.github.com>
2024-04-03 22:54:01 +0530

    Added new Features
    (1).Added Power up :- Fire three bullets at a time in three different directions
    (2).Added Feature for FPS cape so game will run 60 FPS in every devices so no change will be in game speed across different devices
    
    MANY UPDATES WILL BE SOON ADDED

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # Space Defender Project
 
 First python project
-Pygame,Random,Math module and mixer from pygame module are used
+
+Game Elements: The game consists of a player character, enemies (both normal and strong), bullets, power-ups, and health items.
+
+Player Control: The player can move left and right using the arrow keys and shoot bullets using the spacebar.
+
+Enemy Behavior: Normal enemies move downward from random positions at the top of the screen, while strong enemies occasionally appear and move downward. The number and speed of enemies increase as the player's score rises.
+
+Collision Detection: The game detects collisions between bullets and enemies, as well as between the player and enemies. Health items can restore the player's health upon collision.
+
+Scoring System: The player earns points by shooting down enemies. The score increases gradually as the player progresses.
+
+Game Over: The game ends when the player's health reaches zero. A game over screen is displayed, showing the player's final score and providing options to restart or exit the game.
+
+Menu: The game starts with a menu screen where the player can start the game or exit. Background music plays on the menu screen.
+
+
+
+# Changes to the Project
+
+
+(1).Added Power up :- Fire three bullets at a time in three different directions 
+(2).Added Feature for FPS cape so game will run 60 FPS in every devices so no change will be in game speed across different devices
+
+MANY UPDATES WILL BE SOON ADDED.......
+
--- spaceDefenderProject.py
+++ spaceDefenderProject.py
@@ -6,17 +6,18 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
         #Load images
-icon = pygame.image.load("icon.png")
-bg = pygame.image.load("bg2.png")
-playerimage = pygame.image.load("player.png")
-enemyimage = pygame.image.load("enemy.png")
-strongenemyimage = pygame.image.load("strongenemy.png")
-explosionimage = pygame.image.load("explosion.png")
-menuimage = pygame.image.load("menu.png")
-promptimage = pygame.image.load("promt.png")
-endimage = pygame.image.load("end.png")
-endscrbt = pygame.image.load("endscrbt.png")
-healthimage = pygame.image.load("health.png")
+icon = pygame.image.load("icon.png").convert_alpha()
+bg = pygame.image.load("bg2.png").convert_alpha()
+playerimage = pygame.image.load("player.png").convert_alpha()
+enemyimage = pygame.image.load("enemy.png").convert_alpha()
+strongenemyimage = pygame.image.load("strongenemy.png").convert_alpha()
+explosionimage = pygame.image.load("explosion.png").convert_alpha()
+menuimage = pygame.image.load("menu.png").convert_alpha()
+promptimage = pygame.image.load("promt.png").convert_alpha()
+endimage = pygame.image.load("end.png").convert_alpha()
+endscrbt = pygame.image.load("endscrbt.png").convert_alpha()
+healthimage = pygame.image.load("health.png").convert_alpha()
+powerupimage = pygame.image.load("powerup.png").convert_alpha()
 
 # Load sound
 explosionsound = mixer.Sound("explosion.wav")
@@ -30,7 +31,7 @@ gameover = mixer.Sound("gameover.wav")
 playerx = 340
 playery = 540
 playerchange = 0
-playerlevelspeed = 0.3
+playerlevelspeed = 3              #0.3
 playerhealth = 100
 playerhealthx = 470
 playerhealthy = 10
@@ -38,13 +39,23 @@ def playerhealthview(x,y) :
     playerhealthv = font.render("Player Health : " + str(playerhealth) + "%" ,True,(255,255,255))
     screen.blit(playerhealthv,(x,y))
 
+clock = pygame.time.Clock()
+
+#create powerup
+ispowerup = "False"
+player_powered_up = False
+powerup_x =0
+powerup_y =0
+lastscore = 0
+
+
 
         # create diffrent enemies                   
 #noraml enemy : 
 numofnormalenemy = 3   
 normalenemyx =[]
 normalenemyy =[]
-normalenemychange = 0.1                     #for bg(0.1)  for bg1(0.5)
+normalenemychange = 1                     #for bg(0.1)  for bg1(0.5)
 def changenumofnorene() :
     for i in range(numofnormalenemy) :
         normalenemyx.append(random.randint(0,740))
@@ -63,7 +74,44 @@ def checkhealthspawn() :
         chance = random.randint(0,15000)
         if chance == 599 :
             healthspawn = True
+
+checkscore = []
+for i in range(0,10) :
+        checkscore.append(random.randint(i*50,(i+1)*50))
+        checkscore.sort()
+def checkpowerupspawn (checkscore) :
+
+    global ispowerup , powerup_x , powerup_y , player_powered_up, lastscore, numberofbullets
+    if ispowerup == "wait" :
+        numberofbullets = 15
+        if score - lastscore > 20 :
+                ispowerup = "False"
+                powerup_x = 0
+                powerup_y = 0
+                player_powered_up = False
             
+    else :
+        numberofbullets = 5
+        lastscore =  score
+        if ispowerup == "False" :
+            powerup_x = random.randint(10,580)
+            for i in range(0,10) :
+                if round(score) == checkscore[i] :
+                    lastscore = checkscore[i]
+                    ispowerup = "True"
+                
+        if ispowerup == "True" :
+            screen.blit(powerupimage,(powerup_x,powerup_y))
+            powerup_y += 1
+            
+        if powerup_y > 600 :
+            ispowerup = "False"
+            powerup_x = 0
+            powerup_y = 0
+
+
+    
+
 
 def healthspawnob(x,y) :
     if healthspawn == True :
@@ -89,7 +137,7 @@ def normalenemy(x,y) :
 #strong enemy
 strongenemyx = random.randint(0,740)
 strongenemyy = -60
-strongenemychange = 0.15
+strongenemychange = 1.5
 strongenemyhealth = 20
 willstrong = False
 
@@ -106,29 +154,54 @@ pygame.display.set_caption("MY Space Game")
         #create bullet
 bulletx = []
 bullety = []
+bulletp1x = []
+bulletp1y = []
+bulletp2x = []
+bulletp2y = []
+bulletstatep1 = []
+bulletstatep2 = []
 bulletstate = []
 bulletimage = []
-numberofbullets = 5 
-bulletchange = -0.5
+numberofbullets = 15 
+bulletchange = -5
+bulletp1xchange = -1
+bulletp1ychange = -5
+bulletp2xchange = 1
+bulletp2ychange = -5
 for i in range(numberofbullets) :
     bulletx.append(-100)
     bullety.append(550)
+    bulletp1x.append(-100)
+    bulletp1y.append(550)
+    bulletp2x.append(-100)
+    bulletp2y.append(550)
     bulletstate.append("ready")
-    bulletimage.append(pygame.image.load("bullet.png"))
+    bulletstatep1.append("ready")
+    bulletstatep2.append("ready")
+    bulletimage.append(pygame.image.load("bullet.png").convert_alpha())
  
         # collison for every type of enemy with bullet
 # collision for bullet and normal enemy
-def normalenemycollision(xaxis,yaxis,i,j) :
+def normalenemycollision(xaxis,yaxis,i,j,Bullettype) :
     screen.blit(explosionimage,(xaxis,yaxis))
     mixer.Sound.play(explosionsound)
-    global normalenemyx
-    global normalenemyy
+    global normalenemyx, normalenemyy , bulletstate , bulletstatep1 , bulletstatep2 , score
+
     normalenemyx[j] = random.randint(0,740)
     normalenemyy[j] = -60
-    global bulletstate
-    bulletstate[i] = "ready"
-    global score
-    score += 1
+    if Bullettype == "P1" :
+        bulletstatep1[i] = "ready"
+        score += 1
+
+
+    if Bullettype == "P2" :
+        bulletstatep2[i] = "ready"
+        score += 1
+
+
+    if Bullettype == "normal" :
+        bulletstate[i] = "ready"
+        score += 1
 
 
 #collision for bullet and strong enemy
@@ -245,8 +318,24 @@ while running :
                     if bulletstate[i] == "ready" :
                         bulletstate[i] = "fire"
                         mixer.Sound.play(bulletsound)
-                        bulletx[i] = playerx
+                        bulletx[i] = playerx 
                         bullety[i] = playery
+
+                        if player_powered_up == True :
+                            if bulletstatep1[i] == "ready" :
+                                bulletp1x[i] = playerx
+                                bulletp1y[i] = playery
+                                bulletstatep1[i] = "fire"
+
+                                
+                            if bulletstatep2[i] == "ready" :
+                                bulletp2x[i] = playerx 
+                                bulletp2y[i] = playery
+                                bulletstatep2[i] = "fire"
+
+
+
+
                         break
             if event.key == pygame.K_e  :
                 # if event.key == pygame.K_x :
@@ -276,6 +365,15 @@ while running :
         strongenemyy = -60
         willstrong = False
     checkhealthspawn()
+    checkpowerupspawn (checkscore)
+    if ispowerup == "True" :
+        player_and_powerup_dis = math.sqrt(math.pow(playerx - powerup_x ,2) + math.pow(playery-powerup_y ,2 ))
+        if player_and_powerup_dis < 40 :
+            ispowerup = "wait"
+            player_powered_up = True
+            powerup_x = 0
+            powerup_y = 0
+            mixer.Sound.play(healthup)
     healthspawnob(healthx,healthy)
 
     # collision between player and health
@@ -299,24 +397,67 @@ while running :
     #firing bullet
     for i in range(numberofbullets) :
         if bulletstate[i] == "fire" :
-            screen.blit(bulletimage[i],(bulletx[i]+15,bullety[i]+20))
+            screen.blit(bulletimage[i],(bulletx[i]+20,bullety[i]))
             bullety[i] += bulletchange
         if bullety[i] < 0 :
             bulletstate[i] = "ready"
 
         if bulletstate[i] == "ready" :
             bulletx[i] = -100 
+            
+        if bulletstatep1[i] == "fire" :
+            screen.blit(bulletimage[i],(bulletp1x[i]+20,bulletp1y[i]))
+            bulletp1y[i] += bulletp1ychange
+            bulletp1x[i] += bulletp1xchange
+        if bulletp1y[i] < 0 :
+            bulletstatep1[i] = "ready"
+
+        if bulletstatep1[i] == "ready" :
+            bulletp1x[i] = -100
+
+
+        if bulletstatep2[i] == "fire" :
+            screen.blit(bulletimage[i],(bulletp2x[i]+20,bulletp2y[i]))
+            bulletp2y[i] += bulletp2ychange
+            bulletp2x[i] += bulletp2xchange
+        if bulletp2y[i] < 0 :
+            bulletstatep2[i] = "ready"
+
+        if bulletstatep2[i] == "ready" :
+            bulletp2x[i] = -100 
+
 
             #collison between bullet and enemy 
                 # For normal enemy
             
         for j in range(numofnormalenemy) :
-            noramlenemydistance = math.sqrt(math.pow(bulletx[i] - normalenemyx[j] ,2) + math.pow(bullety[i]-normalenemyy[j] ,2 ))
-            if noramlenemydistance < 40 and normalenemyx[j] - bulletx[i] <10 :
+            noramlenemydistance = math.sqrt(math.pow(bulletx[i] - normalenemyx[j] + (enemyimage.get_width()/2)  ,2) + math.pow(bullety[i]-normalenemyy[j] + (enemyimage.get_height()/2) ,2 ))
+            noramlenemydistancep1 = math.sqrt(math.pow(bulletp1x[i] - normalenemyx[j] + (enemyimage.get_width()/2)  ,2) + math.pow(bulletp1y[i]-normalenemyy[j] + (enemyimage.get_height()/2)  ,2 ))
+            noramlenemydistancep2 = math.sqrt(math.pow(bulletp2x[i] - normalenemyx[j] + (enemyimage.get_width()/2) ,2) + math.pow(bulletp2y[i]-normalenemyy[j] + (enemyimage.get_height()/2)  ,2 ))
+            
+
+            if noramlenemydistancep1 < 80 and normalenemyx[j] - bulletp1x[i] <10  :
+                
+                screen.blit(explosionimage,(bulletp1x[i],bulletp1y[i]))
+                normalenemycollisionx = bulletp1x[i]
+                normalenemycollisiony = bulletp1y[i]
+                normalenemycollision(normalenemycollisionx,normalenemycollisiony,i,j,"P1")
+
+
+            if noramlenemydistancep2 < 80 and normalenemyx[j] - bulletp2x[i] <10  :
+                
+                screen.blit(explosionimage,(bulletp2x[i],bulletp2y[i]))
+                normalenemycollisionx = bulletp2x[i]
+                normalenemycollisiony = bulletp2y[i]
+                normalenemycollision(normalenemycollisionx,normalenemycollisiony,i,j,"P2")
+
+
+            if noramlenemydistance < 80 and normalenemyx[j] - bulletx[i] <10  :
+                
                 screen.blit(explosionimage,(bulletx[i],bullety[i]))
                 normalenemycollisionx = bulletx[i]
                 normalenemycollisiony = bullety[i]
-                normalenemycollision(normalenemycollisionx,normalenemycollisiony,i,j)
+                normalenemycollision(normalenemycollisionx,normalenemycollisiony,i,j,"normal")
             
                 ### Player and enemy collision
             playerandnorenemydis = math.sqrt(math.pow(playerx - normalenemyx[j] ,2) + math.pow(playery-normalenemyy[j] ,2 ))
@@ -337,16 +478,16 @@ while running :
             strongenemycollisiony = bullety[i]
             strongenemycollision(strongenemycollisionx,strongenemycollisiony,i)
 
-    if score/200 > 0.1 :
-        normalenemychange = score/200
+    if score/20 > 1 :                            #200/  0.1
+        normalenemychange = score/20
     strongenemychange = 1.2 * normalenemychange
 
-    if normalenemychange > 0.4 :
-        playerlevelspeed = 0.5
-        if normalenemychange > 0.5 :
-            playerlevelspeed = 0.6
-    if normalenemychange < 0.4 :
-        playerlevelspeed = 0.3
+    if normalenemychange > 4 :            #0.4
+        playerlevelspeed = 5              #0.5
+        if normalenemychange > 5 :        #0.5
+            playerlevelspeed = 6          #0.6
+    if normalenemychange < 4 :            #0.4
+        playerlevelspeed = 3              #0.3
     if score/15 > 3 :
         numofnormalenemy = int(score/15)
         changenumofnorene()
@@ -354,7 +495,7 @@ while running :
     scoreview(scorex,scorey)
     playerhealthview(playerhealthx,playerhealthy)
     if healthspawn == True :
-        healthy += 0.1
+        healthy += 1
     playerx += playerchange
     screen.blit(playerimage,(playerx,playery))
     end =False
@@ -375,7 +516,7 @@ while running :
         bgmmusic = False
         men = False
         
-    
+    clock.tick(60)   
     while end :
         screen.fill((0,0,0))
         currscore = endfont.render("Your Score : " + str(yourscore) ,True,(255,255,255))
@@ -399,7 +540,7 @@ while running :
                     running = False
                     menu = False
                     end = False
-                    
+        
         pygame.display.update()
    
     pygame.display.update()
